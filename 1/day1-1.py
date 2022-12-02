86d51a969b6d79b839c92000d5c110f5f988cf8b
Alex Littleton <littale12@gmail.com>
2022-12-01 20:06:27 -0500

    day 1

---
+++ 1/day1-1.py
@@ -0,0 +1,15 @@
+def parseElves():
+    inputFile = open('input.txt', "r")
+    calorieData = inputFile.readlines()
+    inputFile.close()
+    caloriesPerElf = []
+    currentElf = 0
+    for line in calorieData:
+        if line != '\n':
+            currentElf += int(line)
+        else:
+            caloriesPerElf.append(currentElf)
+            currentElf = 0 
+    print(max(caloriesPerElf))
+
+parseElves()
---
+++ 1/day1-2.py
@@ -0,0 +1,20 @@
+def parseElves():
+    inputFile = open('input.txt', "r")
+    calorieData = inputFile.readlines()
+    inputFile.close()
+    caloriesPerElf = []
+    currentElf = 0
+    for line in calorieData:
+        if line != '\n':
+            currentElf += int(line)
+        else:
+            caloriesPerElf.append(currentElf)
+            currentElf = 0 
+
+    sumOfTopThree = 0
+    for i in range(3):
+        sumOfTopThree += max(caloriesPerElf)
+        caloriesPerElf.remove(max(caloriesPerElf))
+    print(sumOfTopThree)
+
+parseElves()
